0ef702c9950f1a71f87ef3b3566762da0bd71dd0
Vlad Sventik <sventik.vlad@gmail.com>
2017-02-11 13:58:33 +0300

    Add new tests and write cod for them

---
+++ src/RomeNumbers.php
@@ -0,0 +1,72 @@
+<?php namespace RomeNumbers;
+
+use InvalidArgumentException;
+
+class RomeNumbers {
+
+
+	protected $romeNumber;
+	protected $arabicEquivalentOfRomeNum = array(
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500, 
+    'M' => 1000 ); 
+
+	public function __construct($romeNumber = null){
+		$this->romeNumber = $romeNumber;
+	}
+
+
+	public function convertRomeNumber() {
+		if ($this->romeNumber == null) { throw new InvalidArgumentException; }
+		return $this->convertRomeToArabic();
+
+	}
+
+
+
+
+	public function setRomeNumber($romeNumber) {
+		$this->checkRomeNumberExceprion($romeNumber);
+		$this->romeNumber = $romeNumber;
+
+	}
+
+
+	protected function checkRomeNumberExceprion($romeNumber) {
+		if ( ! (is_string($romeNumber)) || ($romeNumber == null)) {
+			throw new InvalidArgumentException;
+		}
+		for ($i = 0; $i < strlen($romeNumber); $i++) {
+			if ( ! stristr("IVXLCDM",$romeNumber[$i]) ) {
+				throw new InvalidArgumentException;
+			}
+		}
+	}
+
+
+	protected function convertRomeToArabic() {
+		$lenOfRomeNumber = strlen($this->romeNumber);
+		$sum = 0;
+		$summed = null;
+		for ($i = 0; $i < $lenOfRomeNumber; $i++) {
+			if (($i + 1) < $lenOfRomeNumber) {
+				if ($this->arabicEquivalentOfRomeNum[$this->romeNumber[$i]] < $this->arabicEquivalentOfRomeNum[$this->romeNumber[$i + 1]]) {
+					$sum -= $this->arabicEquivalentOfRomeNum[$this->romeNumber[$i]];
+					$summed = $i +1;
+				} else {
+					
+					$sum += $this->arabicEquivalentOfRomeNum[$this->romeNumber[$i]];	
+					
+				}
+			} else {
+				$sum += $this->arabicEquivalentOfRomeNum[$this->romeNumber[$i]];	
+			}		
+		}	
+		return $sum;
+	}
+
+}
---
+++ tests/RomeNumbersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class LuhnAlgorithmTest extends PHPUnit\Framework\TestCase
+{
+
+	protected $RomeNumbers;
+	
+	protected function setUp() {
+		$this->RomeNumbers = new RomeNumbers\RomeNumbers;
+	}
+
+
+	public function testNumberSetterThrowsExceptionIfInt() {
+		$this->expectException(InvalidArgumentException::class);	
+		$this->RomeNumbers->setRomeNumber(1);
+
+	}
+	public function testNumberSetterThrowsExceptionIfNotRomeNum(){
+		$this->expectException(InvalidArgumentException::class);
+		$this->RomeNumbers->setRomeNumber("XCDa");
+	}
+	
+
+	public function testRomeNumberCorrectConvert() {
+		$this->RomeNumbers->setRomeNumber("XX");
+		$this->assertEquals(20, $this->RomeNumbers->convertRomeNumber()); 
+		$this->RomeNumbers->setRomeNumber("IX");
+		$this->assertEquals(9, $this->RomeNumbers->convertRomeNumber()); 
+		$this->RomeNumbers->setRomeNumber("MMIX");
+		$this->assertEquals(2009, $this->RomeNumbers->convertRomeNumber()); 
+		$this->RomeNumbers->setRomeNumber("MMI");
+		$this->assertEquals(2001, $this->RomeNumbers->convertRomeNumber()); 
+		$this->RomeNumbers->setRomeNumber("XCDDD");
+		$this->assertEquals(1390, $this->RomeNumbers->convertRomeNumber()); 
+		$this->RomeNumbers->setRomeNumber("MID");
+		$this->assertEquals(1499, $this->RomeNumbers->convertRomeNumber()); 
+		
+
+	}
+}
